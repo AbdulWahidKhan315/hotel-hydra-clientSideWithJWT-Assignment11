3fc13f13a8a95131f9db5b50bbf75ae44c04dace
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-11-08 17:44:56 +0600

    added error page with proper design

--- src/pages/ErrorPage/Error.jsx
+++ src/pages/ErrorPage/Error.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import error from '../../assets/error.jpg'
 const Error = () => {
     return (
-        <div>
-            <p className="h-screen flex justify-center items-center text-5xl font-bold">Error: Page not found</p>
+        <div className='flex flex-col-reverse lg:flex-row items-center justify-evenly'>
+            <img className='h-screen border' src={error} alt="" />
+            <Link to='/'>
+                <button className='btn px-20 bg-lime-400'>Go Home</button>
+            </Link>
         </div>
     );
 };
